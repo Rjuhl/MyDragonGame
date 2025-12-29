3fe46c5cb0b68cb0910eddae0f9c52a5b56871d6
Rain Juhl <rainjuhl@gmail.com>
2025-12-29 14:58:18 -0800

    clean up pathfinder class

--- src/world/path_finder.py
+++ src/world/path_finder.py
@@ -1,9 +1,10 @@
-from utils.coords import Coord
-from collections import deque
-from world.astar import AstarManager
 from world.tile import Tile
-from decorators import singleton
+from utils.coords import Coord
+from world.astar import AstarManager
 from constants import PATH_FINDER_CPT
+
+from collections import deque
+from decorators import singleton
 from typing import TYPE_CHECKING, Callable, Optional, List
 
 if TYPE_CHECKING:
@@ -11,18 +12,31 @@ if TYPE_CHECKING:
 
 @singleton
 class PathFinder(AstarManager):
+    """
+        High-level pathfinding manager.
+
+        Adds a small convenience layer on top of AstarManager:
+        - Ensures the destination is within currently-loaded chunk bounds.
+        - If the destination tile is blocked, finds the nearest unblocked tile (BFS).
+        - Enforces a singleton pattern so that it can be called easily from anywhere
+        - PATH_FINDER_CPT dictates the max nodes per tick explored
+    """
+     
     def __init__(self, cycles_per_tick: int):
         super().__init__(cycles_per_tick)
 
     
     def add_job(self, start, destination):
-        return super().add_job(start, self._get_closet_loaded_point(destination))
+        """ Add a pathfinding job, snapping the destination to the closest loaded and unblocked tile. """
+        return super().add_job(start, self._get_closest_loaded_point(destination))
 
 
-    def _get_closet_loaded_point(self, point: Coord) -> Optional[Coord]:
+    def _get_closest_loaded_point(self, point: Coord) -> Optional[Coord]:
+        self._require_map()
+
         point = point.copy()
 
-        # Get loaded boundry from corner chunks 1 and 9
+        # Get loaded bounds from corner chunks 1 and 9
         x_min, y_max, _ = self.map.chunks[0].tiles[0].location.location
         x_max, y_min, _ = self.map.chunks[-1].tiles[-1].location.location
 
@@ -38,24 +52,29 @@ class PathFinder(AstarManager):
 
 
     def _find_closest_unblocked_point(self, point: Coord, is_blocked: Callable[[Tile], bool]) -> Optional[Coord]:
+        """
+            Breadth-first search from `start` (in world coords) to find the closest unblocked tile.
 
-        def get_next_points(coord: Coord, visted: set[Coord]) -> List[Coord]:
+            Uses 4-neighborhood movement (N/S/E/W) in world-tile space.
+        """
+
+        def get_next_points(coord: Coord, visited: set[Coord]) -> List[Coord]:
             coords = [
                 coord.copy().update_as_world_coord(-1, 0),
                 coord.copy().update_as_world_coord(1, 0),
                 coord.copy().update_as_world_coord(0, -1),
                 coord.copy().update_as_world_coord(0, 1)
             ]
-            coords = [c for c in coords if c not in visted]
-            visted.update(coords)
+            coords = [c for c in coords if c not in visited]
+            visited.update(coords)
             return coords
 
-        visted_coords = {point}
+        visited_coords = {point}
         coord_queue = deque([point])
         while coord_queue:
             if (tile := self.map.get_tile(coord_queue.popleft().floor_world())):
                 if not is_blocked(tile): return tile.location
-                coord_queue.extend(get_next_points(tile.location, visted_coords))
+                coord_queue.extend(get_next_points(tile.location, visited_coords))
 
 
     @staticmethod
